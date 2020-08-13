ede19df75821efc68b7468dc554975eca29d2799
Tejas Beri <tejas.beri@transport.nsw.gov.au>
2020-08-13 19:43:38 +1000

    adda scss to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+
+import React from "react";
+import logo from "./logo.svg";
 
 function App() {
   return (
